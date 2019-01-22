d36f0fd567ba19cd06bba17e48b35862285ca7f7
sheetalkumar <sheetal.mca17.du@gmail.com>
2019-01-22 23:52:24 +0530

    text updated

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-import { AppRegistry, Image } from 'react-native';
+import { AppRegistry, View ,Text} from 'react-native';
 
-export default class Bananas extends Component {
+export default class FlexDimensionsBasics extends Component {
   render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
     return (
-      <Image source={pic} style={{width: 550, height: 250}}/>
+      // Try removing the `flex: 1` on the parent View.
+      // The parent will not have dimensions, so the children can't expand.
+      // What if you add `height: 300` instead of `flex: 1`?
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} >
+        <Text style={{fontSize: 23, color: 'black',fontStyle:"normal"
+        ,padding: 26, marginTop:23
+         }}>Hi, I am Sheetal</Text>
+        </View>
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} >
+        <Text style={{fontSize: 23, color: 'black',padding: 26,
+         alignContent :'center'}}>I am android app developer</Text>
+        </View>
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+      </View>
     );
   }
 }
-AppRegistry.registerComponent('AwesomeProject', () => Bananas);
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => FlexDimensionsBasics);
