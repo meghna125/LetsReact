4795fe3f06fe1aec4020828999c0d2056fa2c3be
sheetalkumar <sheetal.mca17.du@gmail.com>
2019-01-23 01:38:13 +0530

    bottom navigation added

--- screens/Home.js
+++ screens/Home.js
@@ -5,7 +5,7 @@ import { Text, View, StyleSheet } from 'react-native';
   render() {
     return (
       <View style={styles.container}>
-        <Text>Home</Text>
+        <Text>Home Sheetal</Text>
       </View>
     );
   }
