b88847cfb69f8251519b77d9fac42619e732f54d
sheetalkumar <sheetal.mca17.du@gmail.com>
2019-01-25 00:25:02 +0530

    new change

--- screens/Home.js
+++ screens/Home.js
@@ -27,7 +27,7 @@
                   
                   <View style={{flex: 1, backgroundColor: 'white', paddingTop: 20}}>
                     <Text style={{fontSize: 24, fontWeight:'700', paddingHorizontal: 20}}>
-                          Hello Megha, What do want to buy?
+                          Hello Megha Kumari, What do want to buy?
                       </Text>
                   <View style={{height: 130, marginTop: 20,}}>
                  
