6499cb81efdb1525dda8ab65d47f69bd8bd0b086
sheetalkumar <sheetal.mca17.du@gmail.com>
2019-01-24 20:49:53 +0530

    scrolling first View added

--- App.js
+++ App.js
@@ -1,71 +1,71 @@
-import React, { Component } from 'react';
-import { AppRegistry, View ,Text,Image,StyleSheet} from 'react-native';
+  import React, { Component } from 'react';
+  import { AppRegistry, View ,Text,Image,StyleSheet} from 'react-native';
 
-import{createBottomTabNavigator} from 'react-navigation'
-import Home from './screens/Home'
-import Profile from './screens/Profile'
-import Cart from './screens/Cart'
-import Fav from './screens/Fav'
+  import{createBottomTabNavigator} from 'react-navigation'
+  import Home from './screens/Home'
+  import Profile from './screens/Profile'
+  import Cart from './screens/Cart'
+  import Fav from './screens/Fav'
 
 
 
-/*
+  /*
   @dev - Sheetal Kumar
   Date - 23 Jan 2019
-*/
+  */
 
-class App extends Component {
+  class App extends Component {
   render() {
-    return (
-      // Try removing the `flex: 1` on the parent View.
-      // The parent will not have dimensions, so the children can't expand.
-      // What if you add `height: 300` instead of `flex: 1`?
-      <View style={{flex: 1}}>
-        
-      </View>
-    );
+  return (
+    // Try removing the `flex: 1` on the parent View.
+    // The parent will not have dimensions, so the children can't expand.
+    // What if you add `height: 300` instead of `flex: 1`?
+    <View style={{flex: 1}}>
+      
+    </View>
+  );
   }
-}
+  }
 
 
-export default createBottomTabNavigator({
+  export default createBottomTabNavigator({
   Home:{
-      screen: Home,
-      navigationOptions: {
-        tabBarLabel: 'Home',
-        tabBarIcon: ({ tintColor }) => (
-          <Image source={require('./assets/house.png')} style={{ height: 24, width: 24, tintColor: tintColor }} />
-        )
-      }
+    screen: Home,
+    navigationOptions: {
+      tabBarLabel: 'Home',
+      tabBarIcon: ({ tintColor }) => (
+        <Image source={require('./assets/house.png')} style={{ height: 24, width: 24, tintColor: tintColor }} />
+      )
+    }
   },
   Fav:{
-    screen: Fav,
-    navigationOptions: {
-      tabBarLabel: 'Favourite',
-      tabBarIcon: ({ tintColor }) => (
-        <Image source={require('./assets/heart.png')} style={{ height: 24, width: 24, tintColor: tintColor }} />
-      )
-    }
-    
+  screen: Fav,
+  navigationOptions: {
+    tabBarLabel: 'Favourite',
+    tabBarIcon: ({ tintColor }) => (
+      <Image source={require('./assets/heart.png')} style={{ height: 24, width: 24, tintColor: tintColor }} />
+    )
+  }
+
   },
   Cart:{
-    screen : Cart,
-    navigationOptions: {
-      tabBarLabel: 'Cart',
-      tabBarIcon: ({ tintColor }) => (
-        <Image source={require('./assets/cart.png')} style={{ height: 24, width: 24, tintColor: tintColor }} />
-      )
-    }
-    
+  screen : Cart,
+  navigationOptions: {
+    tabBarLabel: 'Cart',
+    tabBarIcon: ({ tintColor }) => (
+      <Image source={require('./assets/cart.png')} style={{ height: 24, width: 24, tintColor: tintColor }} />
+    )
+  }
+
   },
   Profile:{
-    screen : Profile,
-    navigationOptions: {
-      tabBarLabel: 'Profile',
-      tabBarIcon: ({ tintColor }) => (
-        <Image source={require('./assets/profile.png')} style={{ height: 24, width: 24, tintColor: tintColor }} />
-      )
-    }
-    
+  screen : Profile,
+  navigationOptions: {
+    tabBarLabel: 'Profile',
+    tabBarIcon: ({ tintColor }) => (
+      <Image source={require('./assets/profile.png')} style={{ height: 24, width: 24, tintColor: tintColor }} />
+    )
   }
-})
+
+  }
+  })
--- screens/Fav.js
+++ screens/Fav.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import { Text, View,StyleSheet } from 'react-native';
+  import React, { Component } from 'react';
+  import { Text, View,StyleSheet } from 'react-native';
 
-/*
-  @dev - Sheetal Kumar
-  Date - 23 Jan 2019
-*/
+  /*
+    @dev - Sheetal Kumar
+    Date - 23 Jan 2019
+  */
 
- class Saved extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Fav</Text>
-      </View>
-    );
-  }
-}
-
-export default Saved;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+  class Saved extends Component {
+    render() {
+      return (
+        <View style={styles.container}>
+          <Text>Fav</Text>
+        </View>
+      );
     }
-});
+  }
+
+  export default Saved;
+
+  const styles = StyleSheet.create({
+      container: {
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center'
+      }
+  });
